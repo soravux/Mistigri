417697935c2712b1a615a1d6c2fefe1aa15c9c65
Yannick Hold <yannickhold@gmail.com>
2015-01-11 10:48:11 -0500

    First idea

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Mistigri
 Multiplayer Web-based Game Engine
+
+
+Setup
+=====
+
+```
+cd server
+npm install
+node .
+```
+THen, go to `client/index.html` with a browser
+
+
+TODO
+====
+
+General
+-------
+
+- Add Grunt
+- Utiliser require()?
+
+Client
+------
+
+- Add Bower
+- Add dependencies using Bower (three.js, socket.io-client, etc)
+
+Server
+------
+
---
+++ server/index.js
@@ -0,0 +1,60 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 31415;
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+// usernames which are currently connected to the chat
+var usernames = {};
+var numUsers = 0;
+var players = {};
+
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  socket.on('update', function (data) {
+    players[socket.username] = data;
+    socket.emit('update', {
+      state: players
+    });
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    // we store the username in the socket session for this client
+    socket.username = username;
+    // add the client's username to the global list
+    usernames[username] = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    // remove the username from global usernames list
+    if (addedUser) {
+      delete usernames[socket.username];
+      delete players[socket.username];
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+});
