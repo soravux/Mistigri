9981971775c632a29561f0bf44ac7b04ccd4d306
Yannick Hold <yannickhold@gmail.com>
2015-01-24 14:34:35 -0500

    Updated build toolchain

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -18,28 +18,48 @@ module.exports = function(grunt) {
     bower_concat: {
       all: {
         dest: 'js/bower.js',
+        exclude: [
+          'ammo.js'
+          // TODO: remove physijs_worker.js
+        ],
         mainFiles: {
-          'ammo.js': ['builds/ammo.js'],
+          //'ammo.js': ['builds/ammo.js'],
           'threejs': ['build/three.js']
         }
       }
     },
     uglify: {
-      bower: {
+      mistigri: {
+        options: {
+          //compress: true,
+          mangle: true
+        },
         files: {
-          'js/mistigri.min.js': ['js/bower.js']
+          'js/mistigri.min.js': ['js/bower.js', 'src/*.js'],
+          'js/ammo.min.js': ['js/ammo.js'],
+          'js/physijs_worker.min.js': ['js/physijs_worker.js']
         }
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {src: ['bower_components/ammo.js/builds/ammo.js'], dest: 'js/ammo.js'},
+          {src: ['bower_components/physijs/physijs_worker.js'], dest: 'js/physijs_worker.js'}
+        ]
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-bower-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   grunt.registerTask('default', ['jshint']);
-  grunt.registerTask('buildbower', [
+  grunt.registerTask('build', [
     'bower_concat',
-    'uglify:bower'
+    'copy',
+    'uglify:mistigri'
   ]);
 
 };
